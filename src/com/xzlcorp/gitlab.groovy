ab324e26fd3ba1f53ff714521678a1e5c52815ad
wuxiaoran <wuxiaoran@xinzhili.cn>
2021-09-23 11:05:57 +0800

    save

--- src/com/xzlcorp/gitlab.groovy
+++ src/com/xzlcorp/gitlab.groovy
@@ -3,7 +3,7 @@ package com.xzlcorp
 //封装HTTP请求
 def HttpReq(reqType,reqUrl,reqBody){
     def gitServer = "https://git.xzlcorp.com/api/v4"
-    withCredentials([string(credentialsId: 'corp-gitlab-jenkins-acesstoken', variable: 'gitlabToken')]) {
+    withCredentials([string(credentialsId: 'corp-git', variable: 'gitlabToken')]) {
       result = httpRequest customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: "${gitlabToken}"]], 
                 httpMode: reqType, 
                 contentType: "APPLICATION_JSON",
