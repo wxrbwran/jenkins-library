8ad9d3cc134bcadd9c445ca4d3fe5e80f6ab677a
wuxiaoran <wuxiaoran@xinzhili.cn>
2021-11-04 15:56:16 +0800

    jenkinsfile

--- vars/websJenkinsfile.groovy
+++ vars/websJenkinsfile.groovy
@@ -82,7 +82,7 @@ def call(params){
             steps {
               script {
                 projects.each({
-                  if (env.GIT_CHANGE.contains(it) && !PNPM_INSTALLED && it != "shared") {
+                  if (env.GIT_CHANGE.contains(it) && !PNPM_INSTALLED) {
                     sh 'npm i -g pnpm --registry=https://registry.npm.taobao.org'
                     sh "pnpm install --registry=https://registry.npm.taobao.org"
                     PNPM_INSTALLED = true
